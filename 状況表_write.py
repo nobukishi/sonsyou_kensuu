ba1b0595e7e2c0b5c7ff90cccb96a36c670207fd
岸信利 <kishinobutoshi@kishinobutoshinoMacBook-Air.local>
2022-07-24 15:54:53 +0900

    すべての所属を検索できた

--- 状況表_write.py
+++ 状況表_write.py
@@ -1,9 +1,10 @@
 import openpyxl
-def find_row_number(ws,key):
+def find_row_number(ws,column_number,key):
     row_number = 0
+    
     for row in ws.iter_rows():
         row_number +=1
-        所属 = row[1].value
+        所属 = row[column_number].value
         if 所属 == None:
             continue
         所属 = 所属.replace('　', '')
@@ -21,12 +22,18 @@ def write_状況表(損傷リスト):
     # セルへ書き込む
     #sheet['D19'] = 損傷リスト['浦和']
     for key in 損傷リスト:
-        row_number = find_row_number(sheet,key)
+        row_number = find_row_number(sheet,1,key) #1はB列のこと
         if row_number == None:
             continue
         cell_number = 'D'+str(row_number)
         sheet[cell_number] = 損傷リスト[key]
-        print(row_number)
-
+        #print(row_number)
+    for key in 損傷リスト:
+        row_number = find_row_number(sheet,23,key) #1はB列のこと
+        if row_number == None:
+            continue
+        cell_number = 'Z'+str(row_number)
+        sheet[cell_number] = 損傷リスト[key]
+    
     # 保存する
     book.save(file_name)
