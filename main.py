9a08708e0b388c2589eee7f822a11a5ba8efe942
岸信利 <kishinobutoshi@kishinobutoshinoMacBook-Air.local>
2022-07-31 16:09:34 +0900

    例外処理

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import 状況表_write
 
 損傷リスト = 損傷リスト_read.get_損傷リスト('損傷リスト.xlsx')
 print(損傷リスト)
-状況表_write.write_状況表(損傷リスト,'状況表.xlsx','４月')
+状況表_write.write_状況表(損傷リスト,'状況表.xlsx','５月')
 
--- 状況表_write.py
+++ 状況表_write.py
@@ -1,3 +1,4 @@
+from tempfile import template
 import openpyxl
 
 def find_row_number(ws,column_number,key):
@@ -9,16 +10,31 @@ def find_row_number(ws,column_number,key):
         if 所属 == None:
             continue
         所属 = 所属.replace('　', '')
-        print(row_number,所属)
+        #print(row_number,所属)
         if key == 所属:
             return row_number 
     
+def copy_sheet(book,src,dst):
+    src_sheet = book[src]
+    book.copy_worksheet(src_sheet)
+    sheet = book[src+' Copy']
+    sheet.title = dst
+    return sheet
+
+def generate_monthly_sheet(book,sheet_name):
+    if sheet_name in book.sheetnames:
+        raise Exception('同じシート名があります')
+    sheet = copy_sheet(book,'テンプレ',sheet_name)
+    sheet['D17']=sheet['D17'].value.replace('month',sheet_name)
+    sheet['J17']=sheet['J17'].value.replace('month',sheet_name)
+    return sheet
     
 def write_状況表(損傷リスト,file_name,sheet_name):
     # ブックを取得
     book = openpyxl.load_workbook(file_name)
+    sheet = generate_monthly_sheet(book,sheet_name)
+    print(book.sheetnames)
     # シートを取得
-    sheet = book[sheet_name]
     # セルへ書き込む
     #sheet['D19'] = 損傷リスト['浦和']
     for key in 損傷リスト:
